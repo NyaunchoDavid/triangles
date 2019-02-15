25c241e3dd208c3aa95c042087e9340f314c18af
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-15 15:49:39 +0300

    add arguments

--- day.js
+++ day.js
@@ -2,18 +2,22 @@ function see(){
   var one = parseInt(document.getElementById('sideOne').value);
   var two = parseInt(document.getElementById('sideTwo').value);
   var three = parseInt(document.getElementById('sideThree').value);
+
+if((one+two>three)&&(one+three>two)&&(three+two>one)){
   if (one===two && two===three) {
     alert("equilateral");
   }
   else if (one===two || one===three || three===two) {
     alert("isosceles");
   }
-  else if(one + two > three){
-    alert("scaline");
-  }
+
   else{
-    alert("not triangle");
+    alert("scalene");
   };
+}
+else{
+  alert("not triangle")
+}
   var pro = document.getElementById('moon');
   var mote = pro.style.backgroundColor = "#2c3e50";
 };
