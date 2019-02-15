b032143dbc43d980e1ca81876ba7f1d52b784209
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-15 10:45:55 +0300

    add html,js,css files

---
+++ day.js
@@ -0,0 +1,16 @@
+function see(){
+  var one = parseInt(document.getElementById('sideOne').value);
+  var two = parseInt(document.getElementById('sideTwo').value);
+  var three = parseInt(document.getElementById('sideThree').value);
+  if (one===two && two===three) {
+    alert("equilateral");
+  }
+  else if (one===two || one===three || three===two) {
+    alert("isosceles");
+  }
+  else{
+    alert("scalene");
+  }
+  var tone = document.getElementById('he');
+  var none = tone.style.fontSize = "24px";
+};
